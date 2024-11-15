af485f0d2651b42cd7df6c4e61760f4dfec0eeea
mab <mab@tutao.de>
2024-11-15 11:02:30 +0100

    Provide info texts for each display option

--- src/common/misc/TranslationKey.ts
+++ src/common/misc/TranslationKey.ts
@@ -791,6 +791,8 @@ export type TranslationKeyType =
 	| "keyManagement.fingerprint_label"
 	| "keyManagement.invalidFingerprint_msg"
 	| "keyManagement.publicKeyFingerprint_label"
+	| "keyManagement.publicKeyFingerprintQrInfo_msg"
+	| "keyManagement.publicKeyFingerprintTextInfo_msg"
 	| "keyManagement.publicKeyFingerprintInfo_msg"
 	| "keyManagement.showFingerprintAs_label"
 	| "keyManagement.unverifiedRecipient_msg"
