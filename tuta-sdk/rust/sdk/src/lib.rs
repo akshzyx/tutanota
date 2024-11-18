fec1aa17e80aabdbd906863e67a8af92f2e43198
nig <nig@tutao.de>
2024-11-18 17:52:02 +0100

    expose instance serialization on LoggedInSdk

--- tuta-sdk/rust/sdk/src/lib.rs
+++ tuta-sdk/rust/sdk/src/lib.rs
@@ -7,6 +7,7 @@ use std::sync::Arc;
 
 use minicbor::encode::Write;
 use minicbor::{Encode, Encoder};
+use serde::Serialize;
 use thiserror::Error;
 
 #[cfg_attr(test, mockall_double::double)]
@@ -22,7 +23,7 @@ use crate::crypto::{aes::Iv, Aes256Key};
 use crate::crypto_entity_client::CryptoEntityClient;
 use crate::date::date_provider::SystemDateProvider;
 use crate::element_value::ElementValue;
-use crate::entities::entity_facade::EntityFacadeImpl;
+use crate::entities::entity_facade::{EntityFacade, EntityFacadeImpl};
 use crate::entities::generated::sys::{CreateSessionData, SaltData};
 use crate::entities::generated::tutanota::Mail;
 #[cfg_attr(test, mockall_double::double)]
@@ -80,6 +81,7 @@ mod typed_entity_client;
 mod user_facade;
 mod util;
 
+use crate::entities::Entity;
 pub use id::custom_id::CustomId;
 pub use id::generated_id::GeneratedId;
 pub use id::id_tuple::IdTupleCustom;
@@ -236,7 +238,11 @@ impl Sdk {
 			service_executor,
 			typed_entity_client,
 			crypto_entity_client,
+			instance_mapper: Arc::clone(&self.instance_mapper),
+			entity_facade,
 			blob_facade,
+			json_serializer: Arc::clone(&self.json_serializer),
+			type_model_provider: Arc::clone(&self.type_model_provider),
 		}))
 	}
 
@@ -340,9 +346,13 @@ pub struct LoggedInSdk {
 	user_facade: Arc<UserFacade>,
 	entity_client: Arc<EntityClient>,
 	service_executor: Arc<ResolvingServiceExecutor>,
+	instance_mapper: Arc<InstanceMapper>,
+	entity_facade: Arc<dyn EntityFacade>,
+	json_serializer: Arc<JsonSerializer>,
 	typed_entity_client: Arc<TypedEntityClient>,
 	crypto_entity_client: Arc<CryptoEntityClient>,
 	blob_facade: Arc<BlobFacade>,
+	type_model_provider: Arc<TypeModelProvider>,
 }
 
 impl LoggedInSdk {
@@ -362,6 +372,44 @@ impl LoggedInSdk {
 			.get_current_sym_group_key(user_group_id)
 			.await
 			.map_err(|err| ApiCallError::internal(format!("KeyLoadError: {err:?}")))
+	}
+
+	pub fn serialize_instance_to_json<Instance>(
+		&self,
+		instance: Instance,
+		key: &GenericAesKey,
+	) -> Result<String, ApiCallError>
+	where
+		Instance: Entity + Serialize,
+	{
+		let type_ref = &Instance::type_ref();
+		let type_model = self
+			.type_model_provider
+			.resolve_type_ref(type_ref)
+			.ok_or_else(|| ApiCallError::InternalSdkError {
+				error_message: format!(
+					"failed to find type model for type ref of instance {}",
+					type_ref
+				),
+			})?;
+		let parsed_instance = self
+			.instance_mapper
+			.serialize_entity(instance)
+			.map_err(|e| ApiCallError::InternalSdkError {
+				error_message: format!("failed to serialize instance {}", type_ref),
+			})?;
+		let encrypted_parsed_entity = if type_model.is_encrypted() {
+			self.entity_facade
+				.encrypt_and_map(type_model, &parsed_instance, key)?
+		} else {
+			parsed_instance
+		};
+		let raw_entity = self
+			.json_serializer
+			.serialize(type_ref, encrypted_parsed_entity)?;
+		serde_json::to_string(&raw_entity).map_err(|e| ApiCallError::InternalSdkError {
+			error_message: format!("failed to stringify raw entity {}", type_ref),
+		})
 	}
 }
 
